84aec1c45551040f42dfaee76938bc60d4afba77
Tiago França <tiago@tiagofranca.com>
2021-12-01 11:52:06 -0300

    seguindo o tutorial do curso de react-native da cod3r
    No ramo 01-evoluindo
    Mudanças a serem submetidas:
    	new file:   android/app/src/main/assets/fonts/AntDesign.ttf
    	new file:   android/app/src/main/assets/fonts/Entypo.ttf
    	new file:   android/app/src/main/assets/fonts/EvilIcons.ttf
    	new file:   android/app/src/main/assets/fonts/Feather.ttf
    	new file:   android/app/src/main/assets/fonts/FontAwesome.ttf
    	new file:   android/app/src/main/assets/fonts/FontAwesome5_Brands.ttf
    	new file:   android/app/src/main/assets/fonts/FontAwesome5_Regular.ttf
    	new file:   android/app/src/main/assets/fonts/FontAwesome5_Solid.ttf
    	new file:   android/app/src/main/assets/fonts/Fontisto.ttf
    	new file:   android/app/src/main/assets/fonts/Foundation.ttf
    	new file:   android/app/src/main/assets/fonts/Ionicons.ttf
    	new file:   android/app/src/main/assets/fonts/MaterialCommunityIcons.ttf
    	new file:   android/app/src/main/assets/fonts/MaterialIcons.ttf
    	new file:   android/app/src/main/assets/fonts/Octicons.ttf
    	new file:   android/app/src/main/assets/fonts/SimpleLineIcons.ttf
    	new file:   android/app/src/main/assets/fonts/Zocial.ttf
    	modified:   android/app/src/main/java/com/awesomeproject/MainActivity.java
    	modified:   android/app/src/main/java/com/awesomeproject/MainApplication.java
    	modified:   android/settings.gradle
    	modified:   ios/AwesomeProject.xcodeproj/project.pbxproj
    	modified:   ios/AwesomeProject/Info.plist
    	modified:   ios/Podfile
    	modified:   package.json
    	modified:   yarn.lock

--- android/app/src/main/java/com/awesomeproject/MainActivity.java
+++ android/app/src/main/java/com/awesomeproject/MainActivity.java
@@ -1,3 +1,4 @@
+import android.os.Bundle;
 package com.awesomeproject;
 
 import com.facebook.react.ReactActivity;
@@ -11,5 +12,10 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "AwesomeProject";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 }
